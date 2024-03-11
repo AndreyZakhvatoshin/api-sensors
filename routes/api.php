72eaa33aa309ebf9ce1b7b9194ca8afa74349bd8
Andrey Zakhvatoshin <mastetandron@gmail.com>
2024-03-11 17:17:40 +0300

    роуты датчиков

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Sensor\SensorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('sensor')
+    ->name('sensor.')
+    ->group(function () {
+        Route::get('temperature', [SensorController::class, 'getTemperature'])->name('temperature');
+        Route::get('pressure', [SensorController::class, 'getPressure'])->name('pressure');
+        Route::get('revolutions', [SensorController::class, 'getRevolutions'])->name('revolutions');
+    });
